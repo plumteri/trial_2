f83559678995a4c099ee599b8f1b02fce1a7ee1f
plumt <plumteri@gmail.com>
2021-08-09 17:00:11 +0900

    8/9 2class commit2_trial2

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articleapp.models import Article
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL,  #연결되어있는 삭제됐을 때 지우진 않고 연결된 게시글이 널이채로 둔다
+                                related_name='comment', null=True)
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               related_name='comment', null=True)
+
+    content = models.TextField(null=False)  # 장문
+
+    created_at =models.DateTimeField(auto_now_add=True) #날짜와 시간

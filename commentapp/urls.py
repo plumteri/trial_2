efb8cc9fa21a68a0fb519a98ff7184e8bcf3c2c9
plumt <plumteri@gmail.com>
2021-08-09 18:22:56 +0900

    8/9 2class commit6_trial2

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from commentapp.views import CommentCreateView
+from commentapp.views import CommentCreateView, CommentDeleteView
 
 app_name = 'commentapp'  #'commentapp:detail'
 
 urlpatterns = [
     path('create/', CommentCreateView.as_view(), name='create'),
-
+    path('delete/<int:pk>', CommentDeleteView.as_view(), name='delete'),
 
 ]

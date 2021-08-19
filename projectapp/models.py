b0bfa80285de53d1a5855af6f55fd9f90ba141b7
plumt <plumteri@gmail.com>
2021-08-19 16:59:08 +0900

    8/19 2class commit1_trial2

--- projectapp/models.py
+++ projectapp/models.py
@@ -8,3 +8,6 @@ class Project(models.Model):
     image = models.ImageField(upload_to='project/', null=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): # 어떤 문자로 되돌려준 것 인지
+        return self.name

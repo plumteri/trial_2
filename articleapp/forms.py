97f2395d476d8bf1109f563a53a8c345e582b757
plumt <plumteri@gmail.com>
2021-08-26 16:59:22 +0900

    8/26 2class commit1_trial2

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,14 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
 
 
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
+                                                           'style': 'min-height: 10red;'
+                                                                    'text-align: left;'}))  # 문자열 입력받는 textarea라는 위젯사용 클래스를 에디터블로 가지는
+
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
--- likeapp/views.py
+++ likeapp/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -12,6 +14,19 @@ from articleapp.models import Article
 from likeapp.models import LikeRecord
 
 
+@transaction.atomic
+def db_transaction(user, article):
+    article.like += 1
+    article.save()
+
+    likeRecord = LikeRecord.objects.filter(user=user,
+                                           article=article)
+    if likeRecord.exists():
+        raise ValidationError('like already exists')
+    else:
+        LikeRecord(user=user, article=article).save()
+
+
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
 
@@ -19,18 +34,14 @@ class LikeArticleView(RedirectView):
         user = request.user  #어떤 유저가
         article = Article.objects.get(pk=kwargs['article_pk'])
 
-        likeRecord = LikeRecord.objects.filter(user=user,
-                                               article=article)  #과거에 좋아요 누른 적있는지
-        if likeRecord.exists():  # 좋아요 누른 적 있으면
-            # 좋아요 반영 x
-            messages.add_message(request, messages.ERROR, '좋아요는 한 번만 가능합니다.')
-            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']}))
-        else:
-            LikeRecord(user=user, article=article).save()
-            article.like += 1
-            article.save()
-            # 좋아요 반영 o
+        try:
+            db_transaction(user, article)
+            # 좋아요 O 메세지 출력
             messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
+        except ValidationError:
+            # 좋아요 X 메세지 출력
+            messages.add_message(request, messages.ERROR, '좋아요는 한번만 가능합니다.')
+            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']}))
 
         return super().get(request, *args, **kwargs)
 
